a4d5f26de107490acc8c2abf7f58b3647fa7cd20
fujitashuichi <fujitashuu14@gmail.com>
2026-04-29 18:14:40 +0900

    fix: signIn関数を切り出し

--- next-app/src/auth/components/SigninForm.tsx
+++ next-app/src/auth/components/SigninForm.tsx
@@ -2,13 +2,13 @@
 
 import { signIn } from "@/auth"
 
+async function handleSignIn() {
+  await signIn("github", { redirectTo: "/" })
+}
+
 export async function SigninForm() {
   return (
-    <form
-      action={async () => {
-        await signIn("github", { redirectTo: "/" })
-      }}
-    >
+    <form action={handleSignIn}>
       <button type="submit">
         Signin with GitHub
       </button>
